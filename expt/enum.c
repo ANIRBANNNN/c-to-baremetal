39ad2733d89800b7b9f38c931e24f8f394f89f75
Anirban Jana <anirban3096@gmail.com>
2026-05-08 13:16:32 +0530

    Enumerators concept added

---
+++ expt/enum.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+enum weekday_e{
+    Sunday , Monday , Tuesday , Wednesday , Thursday , Friday , Saturday 
+};
+void main(){
+    enum weekday_e day = Sunday;
+    printf ("%d\n",Sunday);
+}
