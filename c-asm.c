0c8d3b2e184cacd8291a38e5c4358d13b507df6e
ANIRBAN JANA <anirban3096@gmail.com>
2026-05-22 11:46:21 +0530

    Play with loops in assembly level just by changing c-asm.c

--- c-asm.c
+++ c-asm.c
@@ -1,11 +1,25 @@
 void Anirban(){
-    int i = 10;
+    int i = 3;
     int j = 2;
-    if(i==10){
-        j = j+200;
+    switch (i)
+    {
+    case 1:
+        j=j+1;
+        
+    case 2:
+        j=j+2;
+        break;
+    case 3:
+        j=j+3;
+        break;
+    default:
+        j=j+100;
     }
-    else{
-        j = j-100;
-    }
-    return;
+    // if(i==10){
+    //     j = j+200;
+    // }
+    // else{
+    //     j = j-100;
+    // }
+    // return;
 }
--- c-asm.s
+++ c-asm.s
@@ -11,23 +11,44 @@ Anirban:
 	addi	sp,sp,-32
 	sw	s0,28(sp)
 	addi	s0,sp,32
-	li	a5,10
-	sw	a5,-20(s0)
+	li	a5,3
+	sw	a5,-24(s0)
 	li	a5,2
-	sw	a5,-24(s0)
-	lw	a4,-20(s0)
-	li	a5,10
-	bne	a4,a5,.L2
-	lw	a5,-24(s0)
-	addi	a5,a5,200
-	sw	a5,-24(s0)
-	j	.L1
+	sw	a5,-20(s0)
+	lw	a4,-24(s0)
+	li	a5,3
+	beq	a4,a5,.L2
+	lw	a4,-24(s0)
+	li	a5,3
+	bgt	a4,a5,.L3
+	lw	a4,-24(s0)
+	li	a5,1
+	beq	a4,a5,.L4
+	lw	a4,-24(s0)
+	li	a5,2
+	beq	a4,a5,.L5
+	j	.L3
+.L4:
+	lw	a5,-20(s0)
+	addi	a5,a5,1
+	sw	a5,-20(s0)
+	j	.L6
+.L5:
+	lw	a5,-20(s0)
+	addi	a5,a5,2
+	sw	a5,-20(s0)
 .L2:
-	lw	a5,-24(s0)
-	addi	a5,a5,-100
-	sw	a5,-24(s0)
+	lw	a5,-20(s0)
+	addi	a5,a5,3
+	sw	a5,-20(s0)
+	j	.L6
+.L3:
+	lw	a5,-20(s0)
+	addi	a5,a5,100
+	sw	a5,-20(s0)
 	nop
-.L1:
+.L6:
+	nop
 	lw	s0,28(sp)
 	addi	sp,sp,32
 	jr	ra
